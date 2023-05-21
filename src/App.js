9a869d4266419d2099b0d18624531e067a306385
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-05-21 16:00:24 +0530

    Added Alan AI For Voice Assistant to page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import { useEffect } from 'react';
 import Header from './Components/Header/Header.js';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
 function App() {
+
+  useEffect(() => {
+    alanBtn({
+      key: "f2e6acfac980920c814d6e3b738c99832e956eca572e1d8b807a3e2338fdd0dc/stage",
+      onCommand: ({ command }) => {
+        if (command === "first command") {
+          console.log("First command");
+        }
+      }
+    });
+
+  }, []);
+
+
   return (
     <>
-    <Header />
+      <Header />
     </>
   );
 }
